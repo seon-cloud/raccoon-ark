ee61ef54fcfa7751aed77609bf541d8acfd7a365
Dmitrii Shevelev <igrave1988@gmail.com>
2020-11-26 17:05:00 +0500

    Добавил foxy в выдачу пакета

--- index.mjs
+++ index.mjs
@@ -2,9 +2,16 @@ import ArchibaldClass from './src/Archibald/index.mjs';
 import ConsoleLoggerClass from './src/ConsoleLogger/index.mjs';
 import PistachioClass from './src/Pistachio/index.mjs';
 import RabbitClass from './src/Rabbit/index.mjs';
+import FoxyClass from './src/Foxy/index.mjs';
+import mongoose from 'mongoose';
 
 export const Archibald = ArchibaldClass;
 export const ConsoleLogger = ConsoleLoggerClass;
 export const Pistachio = PistachioClass;
 export const Rabbit = RabbitClass;
 
+export const Foxy = {
+    plugin: FoxyClass,
+    Schema: mongoose.Schema,
+    Types: mongoose.Types
+};
