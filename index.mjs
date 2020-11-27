4014ce5e2696af66130a83b088c53fffb4d7bcad
AlisaPervenenok <48095208+AlisaPervenenok@users.noreply.github.com>
2020-11-27 18:27:31 +0500

    Revert "Простейшая реализация логера Pinocolada"

--- index.mjs
+++ index.mjs
@@ -4,14 +4,11 @@ import PistachioClass from './src/Pistachio/index.mjs';
 import RabbitClass from './src/Rabbit/index.mjs';
 import FoxyClass from './src/Foxy/index.mjs';
 import mongoose from 'mongoose';
-import PinocoladaClass from './src/Pinocolada/index.mjs';
 
 export const Archibald = ArchibaldClass;
 export const ConsoleLogger = ConsoleLoggerClass;
 export const Pistachio = PistachioClass;
 export const Rabbit = RabbitClass;
-export const Pinocolada = PinocoladaClass;
-
 
 export const Foxy = {
     plugin: FoxyClass,
--- src/Pinocolada/constants.mjs
+++
@@ -1,4 +0,0 @@
-/** Имя плагина по умолчанию */
-export const NAME = 'Pinocolada';
-/** Путь для сохранения файла с логами по умолчанию */
-export const DEFAULT_DESTINATION = './logs';
